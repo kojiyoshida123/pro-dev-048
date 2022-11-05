074dd76af1dc8c6f58842e1d2d03228a1316a4b8
kojiyoshida <r2d2.c3po916@outlook.jp>
2022-11-05 14:28:12 +0900

    20221105

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -18,8 +18,8 @@ class CreateFailedJobsTable extends Migration
             $table->string('uuid')->charset("utf8")->unique();
             $table->text('connection');
             $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
+            $table->longText('payload')->charset("utf8");
+            $table->longText('exception')->charset("utf8");
             $table->timestamp('failed_at')->useCurrent();
         });
     }
