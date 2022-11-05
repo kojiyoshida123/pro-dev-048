7f3f617237ff00e643d0a0c14a5992bdd3a29130
kojiyoshida <r2d2.c3po916@outlook.jp>
2022-11-05 14:42:26 +0900

    202211055

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -14,13 +14,6 @@ class CreateFailedJobsTable extends Migration
     public function up()
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->charset("utf8")->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
         });
     }
 
